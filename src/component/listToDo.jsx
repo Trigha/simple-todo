93242930fb8a4d8212785df71c3619b8aabc4ac8
Trigha <adhindyatrigha@gmail.com>
2022-05-20 00:20:32 +0700

    add button new list

--- src/component/listToDo.jsx
+++ src/component/listToDo.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import { HiOutlineTrash, HiOutlineCheck } from 'react-icons/hi';
+import { VscOpenPreview } from 'react-icons/vsc';
 
 function listToDo() {
   return (
     <div className="max-w-xl pt-8 pb-12 mx-auto">
-      <div className="flex items-center justify-between">
-        <h6 className="text-base font-medium text-gray-600">Your list</h6>
-        <p className="text-sm font-light text-gray-500">asd</p>
-      </div>
       <ul className="pt-5">
         <li className="flex flex-row items-start justify-between py-2.5">
           <div className="flex flex-col items-start justify-start gap-x-5">
-            {/* <Link */}
-            {/* // to={`/todo/${item._id}`}
-            // id={item.id}
-            // htmlFor={item.title}
-            > */}
-            {/* {item.isCompleted && ( */}
             <HiOutlineCheck className="inline mr-1.5 text-gray-600" size={14} />
-            {/* )} */}
             <h6 className="text-indigo-600 hover:text-indigo-400 inline transition duration-300 ease-in-out text-base md:text-lg font-medium">
               asd
             </h6>
-
             <p className="text-sm text-gray-500 font-light italic">
               No description
             </p>
             {/* </Link> */}
           </div>
           <div className="flex flex-row items-center gap-x-4">
+            <button
+              type="button"
+              className="transition duration-300 ease-in-out"
+              onClick={() => {
+                console.log('Delete');
+              }}
+            >
+              <VscOpenPreview
+                className="text-gray-600 transition duration-300 ease-in-out rounded-full hover:text-yellow-500"
+                size={20}
+              />
+            </button>
             <button
               type="button"
               className="transition duration-300 ease-in-out"
--- src/component/navHome.jsx
+++ src/component/navHome.jsx
@@ -4,8 +4,13 @@ function NavHome() {
   return (
     <div>
       <div className="mx-auto">
-        <div className="flex justify-center bg-blue-500 w-full h-20">
-          <h1 className="py-6 font-bold text-xl">To Do</h1>
+        <div className="bg-blue-500 w-full h-20">
+          <div className="flex justify-end items-center w-full h-full">
+            <h1 className="absolute left-2/4  py-6 font-bold text-xl">To Do</h1>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-20">
+              Add
+            </button>
+          </div>
         </div>
       </div>
     </div>
