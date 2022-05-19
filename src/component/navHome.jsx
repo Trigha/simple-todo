833e7f705984297b1490dd9ec0738d0fd0ac271f
Trigha <adhindyatrigha@gmail.com>
2022-05-19 22:35:05 +0700

    Navbar home

---
+++ src/component/navHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavHome() {
+  return (
+    <div>
+      <div className="mx-auto">
+        <div className="flex justify-center bg-blue-500 w-full h-20">
+          <h1 className="py-6 font-bold text-xl">To Do</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavHome;
