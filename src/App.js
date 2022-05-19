1845648d4fed2e0176e2eb765f5b0a5ff0ef564e
Trigha <adhindyatrigha@gmail.com>
2022-05-19 23:13:29 +0700

    add loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavHome from './component/navHome';
+import ListToDo from './component/listToDo';
 
 function App() {
   return (
     <div className="App">
       <NavHome />
+      <ListToDo />
     </div>
   );
 }
